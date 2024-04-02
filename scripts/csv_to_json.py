7ce5bc63222869e3b4e1216502cc2db66eae1253
Carlos Bonadeo <carlosbonadeo@gmail.com>
2024-04-03 00:04:25 +0100

    Improve script

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -11,7 +11,7 @@ def csv_to_json(input_csv, output_folder):
     # Contador de citas
     quotes = 0
     # Leer el archivo CSV
-    df = pd.read_csv(input_csv, header=None, names=['time', 'quote_time', 'quote', 'title', 'author', 'sfw', 'star'])
+    df = pd.read_csv(input_csv)
 
     # Rellenar los valores faltantes con texto predeterminado
     df.fillna('', inplace=True)
@@ -21,28 +21,24 @@ def csv_to_json(input_csv, output_folder):
         os.makedirs(output_folder)
 
     # Agrupar por la columna 'time' y crear archivos JSON para cada grupo
-    for key, group in df.groupby('time'):
+    for key, group in df.groupby('Time'):
         # Convertir el DataFrame del grupo a una lista de diccionarios
         data = []
-        for index, row in group.iterrows():
-            # Verificar si quote está vacío
-            if row['quote_time'] == '':
-                continue  # Saltar esta iteración si quote está vacío
-            
-            # Dividir la cita basada en quote_time
-            quote_parts = row['quote'].split(row['quote_time'])
+        for index, row in group.iterrows():            
+            # Dividir la cita basada en Quote time
+            quote_parts = row['Quote'].split(row['Quote time'])
             quote_first = quote_parts[0]
             quote_last = quote_parts[1]
 
             # Construir el diccionario de datos
             entry = {
-                'time': row['time'].replace("_", ":"),
+                'time': row['Time'],
                 'quote_first': quote_first,
                 'quote_last': quote_last,
-                'quote_time_case': row['quote_time'],
-                'title': row['title'],
-                'author': row['author'],
-                'sfw': row['sfw']
+                'quote_time_case': row['Quote time'],
+                'title': row['Title'],
+                'author': row['Author'],
+                'sfw': row['SFW']
             }
             data.append(entry)
         
@@ -50,19 +46,23 @@ def csv_to_json(input_csv, output_folder):
             quotes += 1
 
         # Crear el nombre del archivo JSON
-        json_filename = os.path.join(output_folder, f"{key.replace(":", "_")}.json")
+        json_filename = os.path.join(output_folder, f'{key.replace(':', '_')}.json')
 
         # Escribir los datos en el archivo JSON
         with open(json_filename, 'w') as json_file:
             json.dump(data, json_file, indent=4)
 
+    statistics = {
+        'quotes': quotes,
+        'total': minutes,
+        'missing_quotes': minutes - quotes,
+        'progress': round((quotes * 100) / minutes, 2)
+    }
+
+    print('Statistics:', json.dumps(statistics, indent=4))
+
     with open('statistics.json', 'w') as json_file:
-        json.dump({
-                'quotes': quotes,
-                'total': minutes,
-                'missing_quotes': minutes - quotes,
-                'progress': round((quotes * 100) / minutes, 2)
-            }, json_file, indent=4)
+        json.dump(statistics, json_file, indent=4)
 
 # Ruta al archivo CSV de entrada
 input_csv = 'quotes.csv'
